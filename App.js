8213656c8aca6ad6dbbecfe759c7115c8deeb832
Philip J Spelman <phil_spelman@hotmail.com>
2021-03-15 20:15:06 -0400

    - initial commit with custom header

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header tite={"guess a number"}/>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
