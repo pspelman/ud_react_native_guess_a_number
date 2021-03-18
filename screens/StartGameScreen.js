f78b1e7e814fc3248ec4c55ad5d1a34ce361c5a8
Philip J Spelman <phil_spelman@hotmail.com>
2021-03-17 21:35:28 -0400

    - Card.js   - custom Card component for various card-looking screens - Input.js   - custom Input component for handling text, accepts styling - colors.js   - added constants directory and colors file for the app theme colors - updated input component size and max chars of 2

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import {View, Text, StyleSheet, Button, TextInput} from "react-native";
+import Card from "../components/Card";
+import Colors from "../constants/colors"
+import Input from "../components/Input";
 
 const StartGameScreen = props => {
 	return (
 		<View style={styles.screen}>
 			<Text style={styles.title}>Start a New Game</Text>
-			<View style={styles.inputContainer}>
+			<Card style={styles.inputContainer}>
 				<Text>Select a Number</Text>
-				<TextInput/>
+				<Input style={styles.input}
+				       autoCapitalize={'none'}
+				       autoCorrect={false}
+				       // keyboardType={'numeric'}
+				       keyboardType={'number-pad'}
+				       maxLength={2}
+				/>
 				<View style={styles.buttonContainer}>
-					<Button title="Reset" onPress={()=>{}}/>
-					<Button title="Confirm" onPress={()=>{}}/>
+					<View style={styles.button}>
+						<Button title="Reset" onPress={() => {}} color={Colors.primary}/>
+					</View>
+					<View style={styles.button}>
+						<Button title="Confirm" onPress={() => {}} color={Colors.accent}/>
+					</View>
 				</View>
-			</View>
+			</Card>
 		</View>
 	)
 }
+
 
 const styles = StyleSheet.create({
 	screen: {
@@ -32,22 +46,16 @@ const styles = StyleSheet.create({
 		width: 300,
 		maxWidth: '80%',  // if the device is too small, we make sure the width doesn't exceed 80%
 		alignItems: 'center',
-		shadowColor: 'black',
-		shadowOffset: { width: 0, height: 2},
-		shadowRadius: 6,
-		shadowOpacity: 0.26,
-		elevation: 8,  // this is android only
-		backgroundColor: 'white',
-		padding: 20,
-		borderRadius: 10,
-		// borderBottomLeftRadius: 25,
-		// borderTopRightRadius: 25
 	},
 	buttonContainer: {
 		flexDirection: 'row',
 		width: '100%',  // take up the space of the parent container
 		justifyContent: 'space-between', // distribute all the available free space between them
 		paddingHorizontal: 15  // a little spacing to the left and right of the buttons (so they aren't directly on the edges)
+	},
+	input: {
+		width: 50,
+		textAlign: 'center',
 	}
 })
 
